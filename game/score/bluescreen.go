049e549d700cd60410115f0cdcf00134715404e6
johoermann <johannes.hoermann@tiffinger-thiel.de>
2022-04-03 17:05:54 +0200

    Style the highscore

--- game/score/bluescreen.go
+++ game/score/bluescreen.go
@@ -2,10 +2,13 @@ package score
 
 import (
 	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/aligator/HideAndShell/server"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
@@ -22,6 +25,10 @@ type BluescreenModel struct {
 	highscoreSnapshot []server.Score
 
 	submitMutex sync.Mutex
+
+	highscoreViewport viewport.Model
+
+	ready bool
 }
 
 func NewBlueScreenModel(serverHighscore *server.Highscore) BluescreenModel {
@@ -32,10 +39,10 @@ func NewBlueScreenModel(serverHighscore *server.Highscore) BluescreenModel {
 
 	m := BluescreenModel{
 		serverHighscore: serverHighscore,
-		/*reason:          "some reason",
-		score:       0,*/
-		bsodStyle: lipgloss.NewStyle().Background(lipgloss.Color("#0000ff")).Foreground(lipgloss.Color("#ffffff")).Bold(true),
-		nameInput: nameInput,
+		reason:          "some reason",
+		score:           0,
+		bsodStyle:       lipgloss.NewStyle().Background(lipgloss.Color("#0000ff")).Foreground(lipgloss.Color("#ffffff")).Bold(true),
+		nameInput:       nameInput,
 	}
 
 	return m
@@ -65,6 +72,29 @@ func (m BluescreenModel) Update(msg tea.Msg) (BluescreenModel, tea.Cmd) {
 	case tea.WindowSizeMsg:
 		m.bsodStyle.Width(msg.Width)
 		m.bsodStyle.Height(msg.Height)
+
+		if !m.ready {
+			m.highscoreViewport = viewport.New(0, msg.Height-9)
+			m.highscoreViewport.SetContent("")
+			m.highscoreViewport.GotoTop()
+			m.highscoreViewport.KeyMap = viewport.DefaultKeyMap()
+			m.highscoreViewport.KeyMap.HalfPageUp = key.NewBinding(
+				key.WithKeys("ctrl+u"),
+			)
+			m.highscoreViewport.KeyMap.HalfPageDown = key.NewBinding(
+				key.WithKeys("ctrl+d"),
+			)
+			m.highscoreViewport.KeyMap.Up = key.NewBinding(
+				key.WithKeys("up"),
+			)
+			m.highscoreViewport.KeyMap.Down = key.NewBinding(
+				key.WithKeys("down"),
+			)
+
+			m.ready = true
+		} else {
+			m.highscoreViewport.Height = msg.Height - 9
+		}
 	case activateMsg:
 		m.reason = msg.reason
 	case scoreMsg:
@@ -83,6 +113,18 @@ func (m BluescreenModel) Update(msg tea.Msg) (BluescreenModel, tea.Cmd) {
 					Value: m.score,
 				})
 				m.highscoreSnapshot = m.serverHighscore.Get()
+
+				highscore := ""
+				for i, score := range m.highscoreSnapshot {
+					highscore += strconv.Itoa(i+1) + ": " + strconv.Itoa(score.Value) + " - " + score.Name + "\n"
+				}
+
+				highscore = strings.TrimSuffix(highscore, "\n")
+
+				m.highscoreViewport.SetContent(highscore)
+				m.highscoreViewport.Width = lipgloss.Width(highscore)
+				m.highscoreViewport.GotoTop()
+
 			}
 		}
 	}
@@ -90,24 +132,45 @@ func (m BluescreenModel) Update(msg tea.Msg) (BluescreenModel, tea.Cmd) {
 	m.nameInput, cmd = m.nameInput.Update(msg)
 	cmds = append(cmds, cmd)
 
+	m.highscoreViewport, cmd = m.highscoreViewport.Update(msg)
+	cmds = append(cmds, cmd)
+
 	return m, tea.Batch(cmds...)
 }
 
 func (m BluescreenModel) View() string {
-	if m.reason != "" {
-		msg := "YOU ARE DEAD!!!\n" + m.reason + "\nYour Score: " + strconv.Itoa(m.score)
-		msg += "\n\n"
-
-		if m.highscoreSnapshot == nil {
-			msg += m.nameInput.View() + "\n\n"
-		} else {
-			msg += "Highscore:"
-			for i, score := range m.highscoreSnapshot {
-				msg += strconv.Itoa(i) + ": " + strconv.Itoa(score.Value) + " - " + score.Name + "\n"
-			}
-		}
-		return m.bsodStyle.Render(msg)
+	if m.reason == "" {
+		return ""
 	}
 
-	return ""
+	kernelPanic := "KERNEL PANIC"
+	reason := m.reason
+	score := lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).Render("Your Score: " + strconv.Itoa(m.score))
+	highscore := ""
+	if m.highscoreSnapshot == nil {
+		highscore += m.nameInput.View()
+	} else {
+		highscore += "Highscore:\n"
+	}
+
+	header := lipgloss.JoinVertical(lipgloss.Center, kernelPanic, reason, score, highscore)
+
+	headerWidth := lipgloss.Width(header)
+	header = lipgloss.NewStyle().
+		MarginLeft(m.bsodStyle.GetWidth()/2 - headerWidth/2).
+		Render(header)
+
+	msg := header + "\n"
+
+	if m.highscoreSnapshot != nil {
+		board := lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).Padding(0, 1).Render(m.highscoreViewport.View())
+
+		board = lipgloss.NewStyle().
+			MarginLeft(m.bsodStyle.GetWidth()/2 - lipgloss.Width(board)/2).
+			Render(board)
+
+		msg += board
+	}
+
+	return m.bsodStyle.Render(msg)
 }
